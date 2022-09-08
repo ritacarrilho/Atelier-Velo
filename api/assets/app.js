7d39a80ac513ed2a43e2c032b53feb8471b03d99
ritacarrilho <ritaclameira@gmail.com>
2022-09-08 09:35:30 +0200

    Webpack and easyadmin bundle for backoffice installation

--- api/assets/app.js
+++ api/assets/app.js
@@ -8,28 +8,4 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-console.log('4444');
-
-(function($) {
-    $(function() {
-      $('nav ul li > a:not(:only-child)').click(function(e) {
-        $(this)
-          .siblings('.nav-dropdown')
-          .slideToggle()
-        $('.nav-dropdown')
-          .not($(this).siblings())
-          .hide()
-        e.stopPropagation()
-      })
-      $('html').click(function() {
-        $('.nav-dropdown').hide()
-      })
-      // Toggle open and close nav styles on click
-      $('#nav-toggle').click(function() {
-        $('nav ul').slideToggle();
-      });
-      $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active')
-      })
-    })
-  })
+console.log('start');
