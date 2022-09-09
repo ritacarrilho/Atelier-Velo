379b17cfbdc109e97d0a4d17c9d6f24fd449c181
ritacarrilho <ritaclameira@gmail.com>
2022-09-09 16:33:01 +0200

    dashboard menu changes

--- api/src/Controller/Admin/DashboardController.php
+++ api/src/Controller/Admin/DashboardController.php
@@ -12,36 +12,44 @@ use App\Entity\ProductCategory;
 use App\Entity\Subscriber;
 use App\Entity\SubscriberRole;
 use App\Entity\User;
+use App\Repository\BicycleRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\SubscriberRepository;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class DashboardController extends AbstractDashboardController
 {
-      /**
+    /**
      * @var SubscriberRepository
      */
     private $subscriberRepo;
 
-    public function __construct(SubscriberRepository $subscriberRepository)
+    /**
+     * @var BicycleRepository
+     */
+    private $bicycleRepo;
+
+    public function __construct(SubscriberRepository $subscriberRepository, BicycleRepository $bicycleRepository)
     {
         $this->subscriberRepo = $subscriberRepository;
+        $this->bicycleRepo = $bicycleRepository;
+
     }
+
     /**
      * @Route("/admin", name="admin")
      */
     public function index(): Response
     {
         $subscribers = $this->subscriberRepo->findAll();
-
+        $bicycles = $this->bicycleRepo->findByDisponibility('0');
 
         return $this->render('admin/dashboard.html.twig', [
             'subscribers' => count($subscribers),
+            'bicycles' => count($bicycles),
         ]);
     }
 
@@ -51,24 +59,27 @@ class DashboardController extends AbstractDashboardController
             ->setTitle('Atelier Vélo du Vernet');
     }
 
-    // public function configureCrud(string $pageName): Crud
-    // {
-    //     $pageName = "Vélos";
-    //     return Crud::new()->setPageTitle($pageName);
-    // }
-    
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToCrud('Vélo', 'fas fa-bicycle', Bicycle::class);
-        yield MenuItem::linkToCrud('Vélo taille', 'fas fa-bicycle', BicycleSize::class);
-        yield MenuItem::linkToCrud('Vélo type', 'fas fa-bicycle', BicycleType::class);
-        yield MenuItem::linkToCrud('Evénements', 'fas fa-calendar-alt', Event::class);
-        yield MenuItem::linkToCrud('Catégorie des evénements', 'fas fa-calendar-alt', EventCategory::class);
-        yield MenuItem::linkToCrud('Produits', 'fas fa-store', Product::class);
-        yield MenuItem::linkToCrud('Catégorie des produits', 'fas fa-store', ProductCategory::class);
-        yield MenuItem::linkToCrud('Adhérents', 'fas fa-biking', Subscriber::class);
-        yield MenuItem::linkToCrud('Role des Adhérents', 'fas fa-user-friends', SubscriberRole::class);
-        yield MenuItem::linkToCrud('Comptes', 'fas fa-user', User::class);
-        yield MenuItem::linktoUrl('Accueil', 'fas fa-home', $this->generateUrl('app_home'));
+        return [
+            MenuItem::linktoUrl('Accueil', 'fas fa-home', $this->generateUrl('app_home')),
+
+            // MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+            MenuItem::section('Vélos et Produits'),
+            MenuItem::linkToCrud('Vélos', 'fas fa-bicycle', Bicycle::class),
+            MenuItem::linkToCrud('Vélo taille', 'fas fa-bicycle', BicycleSize::class),
+            MenuItem::linkToCrud('Vélo type', 'fas fa-bicycle', BicycleType::class),
+            MenuItem::linkToCrud('Produits', 'fas fa-store', Product::class),
+            MenuItem::linkToCrud('Catégorie des produits', 'fas fa-store', ProductCategory::class),
+
+            MenuItem::section('Evénements'),
+            MenuItem::linkToCrud('Evénements', 'fas fa-calendar-alt', Event::class),
+            MenuItem::linkToCrud('Catégorie des evénements', 'fas fa-calendar-alt', EventCategory::class),
+
+            MenuItem::section('Adhérents et Utilisateurs'),
+            MenuItem::linkToCrud('Adhérents', 'fas fa-biking', Subscriber::class),
+            MenuItem::linkToCrud('Role des Adhérents', 'fas fa-user-friends', SubscriberRole::class),
+            MenuItem::linkToCrud('Comptes', 'fas fa-user', User::class),
+        ];
     }
 }
--- api/src/Repository/BicycleRepository.php
+++ api/src/Repository/BicycleRepository.php
@@ -42,17 +42,15 @@ class BicycleRepository extends ServiceEntityRepository
 //    /**
 //     * @return Bicycle[] Returns an array of Bicycle objects
 //     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('b')
-//            ->andWhere('b.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('b.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+   public function findByDisponibility($value): array
+   {
+       return $this->createQueryBuilder('b')
+           ->andWhere('b.disponibility = :val')
+           ->setParameter('val', $value)
+           ->getQuery()
+           ->getResult()
+       ;
+   }
 
 //    public function findOneBySomeField($value): ?Bicycle
 //    {
