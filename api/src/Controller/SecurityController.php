26788e6c46d36291b89a43c5bade6639dd63818e
ritacarrilho <ritaclameira@gmail.com>
2022-09-14 12:08:56 +0200

    front part creation

--- api/src/Controller/SecurityController.php
+++ api/src/Controller/SecurityController.php
@@ -26,18 +26,6 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    // /**
-    //  * @Route("/api/login", name="api_login", methods={"POST"}) 
-    //  */
-    // public function apiLogin() {
-    //     $user=$this->getUser();
-
-    //     return $this->json([
-    //         'username' => $user->getUsername(),
-    //         'roles' => $user->getRoles()
-    //     ]);
-    // }
-
     /**
      * @Route("/logout", name="app_logout")
      */
